5f37ba9eab417471f15b38e045ba182b86eee7a3
Odii Daniel <xenithheight@gmail.com>
2025-03-19 22:17:15 +0100

    update

--- api/routes/visaRoutes.js
+++ api/routes/visaRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { conductVisaInterview } = require('../controllers/visaController');
+const { conductVisaInterview, conductVisaInterviewForAuthUsers } = require('../controllers/visaController');
 const router = express.Router();
 
 // Visa interview route
 router.post('/question', conductVisaInterview);
+router.post('/auth/question', conductVisaInterviewForAuthUsers);
 
 module.exports = router; // ✅ Ensure this exports the router correctly
